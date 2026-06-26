b9e79d4942bdee8c16a594c446125d0fca614a2b
AleixMJ <aleixtechIT@gmail.com>
2026-06-26 17:22:17 +0200

    feat: added grid to the Gameboard constructor and also the method placeShip() on the board

--- Gameboard.js
+++ Gameboard.js
@@ -1,4 +1,35 @@
+import Ship from "./Ship";
+
 class Gameboard {
+    constructor() {
+        this.grid = Array.from({ length: 10}, () => Array(10).fill(null));
+    }
+
+    placeShip(length,  startY, startX, vertical = true) {
+        let ship = new Ship(length);
+        if (vertical === true) {
+
+            if(startY + ship.length > 10) throw Error("ship out of boundries")
+
+            this.grid[startY][startX] = ship;
+            let SquaresOccupied = 1;
+            while (ship.length > SquaresOccupied ) {
+                startY++;
+                this.grid[startY][startX] = ship;
+                SquaresOccupied++;
+            }
+        } else {
+            if(startX + ship.length > 10) throw Error("ship out of boundries")
+
+            this.grid[startY][startX] = ship;
+            let SquaresOccupied = 1;
+            while (ship.length > SquaresOccupied ) {
+                startX++;
+                this.grid[startY][startX] = ship;
+                SquaresOccupied++;
+            }
+        }
+    }
 
 };
 
---
+++ gameboard.test.js
@@ -0,0 +1,24 @@
+import Gameboard from "./Gameboard.js";
+import Ship from "./Ship.js";
+
+describe("Gameboard Mechanics", () => {
+
+    let testGameboard;
+
+    beforeEach(() => {
+        testGameboard = new Gameboard;
+    });
+
+    test('Gameboard has a 10x10 matrix', () => {
+        expect(testGameboard.grid[0][0]).toBe(null);
+        expect(testGameboard.grid[9][9]).toBe(null);
+        expect(testGameboard.grid[10]?.[10]).toBe(undefined);
+        expect(testGameboard.grid[15]?.[12]).toBe(undefined);
+    }); 
+    
+    test('Ship is placed on board', () => {
+        testGameboard.placeShip(1, 2, 2);
+        expect(testGameboard.grid[2][2]).toBeInstanceOf(Ship);
+    }); 
+
+});
