b3b7407fc2159c71a4fdb46880a0a9ccabcfdcfe
AleixMJ <aleixtechIT@gmail.com>
2026-06-26 15:17:17 +0200

    feat: implement core Ship class and hit tracking logic

--- Ship.js
+++ Ship.js
@@ -1,5 +1,15 @@
 class Ship {
-
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+    }
+    
+    hit() {
+        if (this.hits == this.length) {
+            return
+        }
+        this.hits ++;
+    }
 };
 
 export default Ship;
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,38 @@
-import Ship from './Ship.js';
+import Ship from './Ship.js'
 
-test('Jest setup health check', () => {
-    expect(true).toBe(true);
+describe("Ship Mechanics", () => {
+
+    let testShip;
+    beforeEach(() => {
+        testShip = new Ship(3);
+    })
+
+    test('Initialises with 0 hits', () => {
+        expect(testShip.hits).toBe(0);
+    });
+
+
+    test('Ship length', () => {
+
+        expect(testShip.length).toBe(3);
+    });
+
+    test('Ship was hit', () => {
+        
+        testShip.hit();
+        expect(testShip.hits).toBe(1);
+        testShip.hit();
+        expect(testShip.hits).toBe(2);
+    });
+
+    test('Ship hits cannot be higher than ship length', () => 
+    {
+        testShip.hit();
+        testShip.hit();
+        testShip.hit();
+        testShip.hit();
+        expect(testShip.hits).toBe(3);
+        
+    });
+
 });
